e07e5e793ea8b16b04cb8613205ac6a7c6c69e70
lloydDevs <lloyd11ramirez@gmail.com>
2026-09-20 19:34:59 +0800

    refactor: remove unused web and auth boilerplate

--- app/Modules/Product/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Modules\Product\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['web', 'auth'])->group(function () {
-    Route::resource('products', ProductController::class);
-});
--- tests/Feature/ExampleTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-test('the application redirects the root to the login or dashboard', function () {
-    $response = $this->get('/');
-
-    $response->assertRedirect('/dashboard');
-});
