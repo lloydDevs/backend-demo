2c62ae1be498de19f86433b04a928f56aaf3f65b
lloydDevs <lloyd11ramirez@gmail.com>
2026-09-20 20:57:49 +0800

    fix: enable API route model binding

--- app/Modules/Booking/routes.php
+++ app/Modules/Booking/routes.php
@@ -5,7 +5,7 @@ use App\Modules\Booking\Http\Controllers\Api\CustomerController;
 use App\Modules\Booking\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api/v1')->middleware('throttle:30,1')->group(function () {
+Route::prefix('api/v1')->middleware(['api', 'throttle:30,1'])->group(function () {
     Route::get('services', [ServiceController::class, 'index']);
     Route::get('customers', [CustomerController::class, 'index']);
     Route::apiResource('bookings', BookingController::class);
